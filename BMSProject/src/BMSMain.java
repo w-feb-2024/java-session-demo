6ac62e98668a4bc9c04f264a74e86b9d4f7344cf
bpoongs <bpoongs@gmail.com>
2024-02-27 16:53:16 +0530

    BMS service layer

---
+++ BMSProject/src/BMSMain.java
@@ -0,0 +1,9 @@
+
+public class BMSMain {
+
+	public static void main(String[] args) {
+		// this is the starting point of your project
+
+	}
+
+}
---
+++ BMSProject/src/service/BookServiceImpl.java
@@ -0,0 +1,52 @@
+package service;
+
+import java.util.List;
+import java.util.Optional;
+
+import dao.BookDao;
+import dao.BookDaoCollectionImpl;
+import model.BookPojo;
+
+public class BookServiceImpl implements BookService{
+
+	BookDao bookDao;
+	
+	public BookServiceImpl() {
+		bookDao = new BookDaoCollectionImpl();
+	}
+
+	@Override
+	public List<BookPojo> fetchAllBook() {
+		List<BookPojo> allBooks = bookDao.fetchAllBook();
+		return allBooks;
+	}
+
+	@Override
+	public Optional<BookPojo> fetchABook(int bookId) {
+		Optional<BookPojo> bookPojoOptional = bookDao.fetchABook(bookId);
+		return bookPojoOptional;
+	}
+
+	@Override
+	public List<BookPojo> fetchByBookGenre(String genre) {
+		List<BookPojo> allBookGenres = bookDao.fetchByBookGenre(genre);
+		return allBookGenres;
+	}
+
+	@Override
+	public void removeBook(int bookId) {
+		bookDao.removeBook(bookId);
+	}
+
+	@Override
+	public BookPojo addBook(BookPojo newBook) {
+		BookPojo book = bookDao.addBook(newBook);
+		return book;
+	}
+
+	@Override
+	public BookPojo updateBook(BookPojo updateBook) {
+		BookPojo book = bookDao.updateBook(updateBook);
+		return book;
+	}
+}
